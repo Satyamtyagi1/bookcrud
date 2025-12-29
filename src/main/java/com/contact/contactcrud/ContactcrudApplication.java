058d52d71a17983f83f7b23eb783969ed24dc889
Satyamtyagi1 <satyamtyagi80064@gmail.com>
2025-12-29 12:59:04 +0530

    Add src/main/java/com/contact/contactcrud/ContactcrudApplication.java

---
+++ src/main/java/com/contact/contactcrud/ContactcrudApplication.java
@@ -0,0 +1,12 @@
+package com.contact.contactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactcrudApplication.class, args);
+    }
+}
